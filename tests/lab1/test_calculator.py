6af3e88afdf460249ed04e1a3b2ceb1b96a943b9
XZ1BIT2BY <144528154+XZ1BIT2BY@users.noreply.github.com>
2023-10-26 15:39:01 +0300

    Update test_calculator.py

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,43 @@
-import unittest
+import random
 
-class CalculatorTestCase(unittest.TestCase):
+def addition(x, y):
+    return x + y
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: division by zero"
+
+def generate_random_numbers():
+    return random.uniform(-100, 100), random.uniform(-100, 100)
+
+def test_calculator():
+    for _ in range(10):  # Run 10 random tests
+        num1, num2 = generate_random_numbers()
+        
+        # Test addition
+        assert addition(num1, num2) == num1 + num2
+
+        # Test subtraction
+        assert subtraction(num1, num2) == num1 - num2
+
+        # Test multiplication
+        assert multiplication(num1, num2) == num1 * num2
+
+        # Test division
+        if num2 != 0:
+            assert division(num1, num2) == num1 / num2
+        else:
+            assert division(num1, num2) == "Error: division by zero"
+
+    print("All tests pass!")
+
+# Run the automated test function
+test_calculator()
