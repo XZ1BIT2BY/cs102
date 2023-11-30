eea1121b6c26c586839d281e6a5b06b3b82dd2ab
XZ1BIT2BY <144528154+XZ1BIT2BY@users.noreply.github.com>
2023-11-30 13:52:15 +0300

    Create test_vigenre.py

---
+++ tests/lab2/test_vigenre.py
@@ -0,0 +1,10 @@
+import random, string, unittest
+from src.lab2.vigenere import decrypt_vigenere, encrypt_vigenere
+
+class TestVigenre(unittest.TestCase):
+    def test_randomized(self):
+        kwlen = random.randint(4, 24)
+        keyword = ''.join(random.choice(string.ascii_letters) for _ in range(kwlen))
+        plaintext = ''.join(random.choice(string.ascii_letters + ' -,') for _ in range(64))
+        ciphertext = encrypt_vigenere(plaintext, keyword)
+        self.assertEqual(plaintext, decrypt_vigenere(ciphertext, keyword))
