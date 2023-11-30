7f745cbc5fc4305d328645ec9fe3a0903e51dd4d
XZ1BIT2BY <144528154+XZ1BIT2BY@users.noreply.github.com>
2023-11-30 13:47:03 +0300

    Update vigenre.py

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,3 +1,4 @@
+import random, string, vigenere
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +10,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    keyword = keyword.upper()  # Приводим ключевое слово к верхнему регистру для унификации
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isalpha():
+            # Определяем, является ли символ буквой
+            start = ord('A') if char.isupper() else ord('a')
+            # Определяем начальный код для заглавных и строчных букв
+            key_char = keyword[i % len(keyword)]  # Получаем символ ключа для текущей позиции
+            key_shift = ord(key_char) - ord('A') if key_char.isupper() else ord(key_char) - ord('a')
+            # Вычисляем сдвиг для символа ключа
+            encrypted_char = chr((ord(char) - start + key_shift) % 26 + start)
+            # Шифруем символ и добавляем его к результату
+            ciphertext += encrypted_char
+        else:
+            # Оставляем символы, не являющиеся буквами, без изменений
+            ciphertext += char
+
+
     return ciphertext
 
 
@@ -24,5 +44,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    keyword = keyword.upper()  # Приводим ключевое слово к верхнему регистру для унификации
+
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isalpha():
+            # Определяем, является ли символ буквой
+            start = ord('A') if char.isupper() else ord('a')
+            # Определяем начальный код для заглавных и строчных букв
+            key_char = keyword[i % len(keyword)]  # Получаем символ ключа для текущей позиции
+            key_shift = ord(key_char) - ord('A') if key_char.isupper() else ord(key_char) - ord('a')
+            # Вычисляем сдвиг для символа ключа
+            decrypted_char = chr((ord(char) - start - key_shift) % 26 + start)
+            # Дешифруем символ и добавляем его к результату
+            plaintext += decrypted_char
+        else:
+            # Оставляем символы, не являющиеся буквами, без изменений
+            plaintext += char
+
+
+
     return plaintext
+
